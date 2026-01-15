895296f7b37d1b1220c1b139b51a9dbd50a61a56
sbg93 <sergio.sbg93@gmail.com>
2026-01-15 21:52:01 +0100

    Create Godot project skeleton

---
+++ scripts/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+const TARGET_SCORE := 10
+
+var current_score := 0
+
+func _ready() -> void:
+	print("Endless Bullseye ready. Target score: %d" % TARGET_SCORE)
+	_update_title()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		add_point()
+
+func add_point() -> void:
+	current_score = clampi(current_score + 1, 0, TARGET_SCORE)
+	_update_title()
+
+func _update_title() -> void:
+	var label := $WelcomeLabel
+	if label is Label:
+		label.text = "Endless Bullseye\nPuntos: %d / %d" % [current_score, TARGET_SCORE]
